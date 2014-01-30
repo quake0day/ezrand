dae8e2567921b892e4700ecba047119ad9a425f3
quake0day <quake0day@gmail.com>
2014-01-30 12:35:43 -0500

    new version, change data process function

--- src/read.py
+++ src/read.py
@@ -2,7 +2,7 @@ import csv
 import hashlib
 import binascii
 import string
-FILENAME = "../data/w001day1-raw.csv"
+FILENAME = "../data/w001day2-raw.csv"
 
 def hex2bin(str):
    bin = ['0000','0001','0010','0011',
@@ -17,28 +17,36 @@ def hex2bin(str):
 
 
 
-with open(FILENAME, 'rb') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar= '|')
-    newFILENAME = FILENAME.split(".csv")[0]+".bin"
-    f = open(newFILENAME,'w')
-    i = 0
-    for row in spamreader:
-        for cell in row:
-            #print cell.split("\t")[1]  # accel_x
-            accel_x = cell.split("\t")[1]  # accel_x
-            accel_y = cell.split("\t")[2]  # accel_y
-            accel_y = cell.split("\t")[9]  # accel_y
-            #print accel_y
-            if accel_y[-6:] != '-0.0':
-                print accel_y[-6:]
-                hash_res = hashlib.sha1(accel_y[-7:]).hexdigest()
-                bindata = binascii.unhexlify(hash_res)
-            #hex2bin(hash_res)
-                if i == 0:
-                    f.write(bindata)
-                    i = i + 1
-                if i == 1:
-                    i = 0
-    f.close()
+# kind: 1 for accel_x 
+#       2 for accel_y.....
+# Input : *.csv
+# Output: *.bin at the same directory 
+# Step: jump every x data 
+# Like Step= 1, then data with index 0, 2, 4,...  will be processed
+def process_data(FILENAME, kind, step):
+    with open(FILENAME, 'rb') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar= '|')
+        newFILENAME = FILENAME.split(".csv")[0]+".bin"
+        f = open(newFILENAME,'w')
+        i = 0
+        for row in spamreader:
+            for cell in row:
+                #print cell.split("\t")[1]  # accel_x
+                rawdata = cell.split("\t")[kind]  # accel_y
+                #print accel_y
+
+                # if 0 then drop
+                if rawdata[-6:] != '-0.0':
+                    print rawdata[-6:]
+                    hash_res = hashlib.sha1(rawdata[-7:]).hexdigest()
+                    bindata = binascii.unhexlify(hash_res)
+                #hex2bin(hash_res)
+                    if i == 0:
+                        f.write(bindata)
+                        i = i + 1
+                    if i == step:
+                        i = 0
+        f.close()
             
             
+process_data(FILENAME, 2, 1)
