74ea62dbdf9420145d371d190995dbe5bf3cde58
quake0day <quake0day@gmail.com>
2014-01-30 11:25:20 -0500

    add entropy cal script

--- src/read.py
+++ src/read.py
@@ -16,6 +16,7 @@ def hex2bin(str):
 
 
 
+
 with open(FILENAME, 'rb') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar= '|')
     newFILENAME = FILENAME.split(".csv")[0]+".bin"
@@ -25,15 +26,18 @@ with open(FILENAME, 'rb') as csvfile:
         for cell in row:
             #print cell.split("\t")[1]  # accel_x
             accel_x = cell.split("\t")[1]  # accel_x
-            if accel_x[-6:] != '-0.0':
-                print accel_x[-6:]
-                hash_res = hashlib.sha1(accel_x[-7:]).hexdigest()
+            accel_y = cell.split("\t")[2]  # accel_y
+            accel_y = cell.split("\t")[9]  # accel_y
+            #print accel_y
+            if accel_y[-6:] != '-0.0':
+                print accel_y[-6:]
+                hash_res = hashlib.sha1(accel_y[-7:]).hexdigest()
                 bindata = binascii.unhexlify(hash_res)
             #hex2bin(hash_res)
                 if i == 0:
                     f.write(bindata)
                     i = i + 1
-                if i == 3:
+                if i == 1:
                     i = 0
     f.close()
             
